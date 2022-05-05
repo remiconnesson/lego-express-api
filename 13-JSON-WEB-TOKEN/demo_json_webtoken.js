b7b6b8e5f48e60cdc3d0bb03cb7e5e010c297caa
Rémi Connesson <remiconnesson@gmail.com>
2022-05-05 16:39:27 +0200

    Signer et vérifier un token.

---
+++ 13-JSON-WEB-TOKEN/demo_json_webtoken.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+
+
+const token = jwt.sign({userId: 12}, process.env.SECRET_JWT);
+console.log(token);
+
+const decoded = jwt.verify(token, process.env.SECRET_JWT);
+console.log(decoded);
+
+try {
+  const notdecoded = jwt.verify(token, "PAS_LE_BON_SECRET");
+}catch(exc){
+  console.log('PAS LA BONNE SIGNATURE');
+}
+
+
+
