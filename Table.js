24cbe5c4118f2954355898d9f3df4d7c5bddfc55
Rémi Connesson <remiconnesson@gmail.com>
2022-05-05 19:26:40 +0200

    Encapsule la toy DB pour créer plusieurs Tables

---
+++ Table.js
@@ -0,0 +1,35 @@
+class Table {
+  constructor(tableName) {
+    this.tableName = tableName;
+    this.memoryDb = new Map();
+    this.id = 0;
+  }
+
+  insertOne(obj) {
+    this.memoryDb.set(this.id++, obj);
+
+  }
+  exists(id) {
+    return this.memoryDb.has(id);
+  }
+  updateOne(id, obj) {
+    if (this.exists(id)) {
+      this.memoryDb.set(id, obj);
+    } else {
+      throw new Error(`Key ${id} doesn't not exists`);
+    }
+  }
+  deleteOne(id) {
+    if (this.exists(id)) {
+      this.memoryDb.delete(id);
+    } else {
+      throw new Error(`Key ${id} doesn't not exists`);
+    }
+  }
+
+  getAll() {
+    return Object.fromEntries(this.memoryDb);
+  }
+}
+
+module.exports = Table;
