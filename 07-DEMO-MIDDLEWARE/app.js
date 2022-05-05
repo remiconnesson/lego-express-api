b5a547aa2ebd0ea7038bb792a54245a05f64dc2d
Rémi Connesson <remiconnesson@gmail.com>
2022-05-05 12:05:03 +0200

    Illustration de l'ordre d'appel des middlewares

--- 07-DEMO-MIDDLEWARE/app.js
+++ 07-DEMO-MIDDLEWARE/app.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const app = express()
 
-// définition du middleware
-function logRequest(req, res, next) {
-	console.log(req.method, req.url);
+// définition des middlewares
+function logRequest1(req, res, next) {
+	console.log("#1", req.method, req.url);
 	next();
 }
 
-// utilisation du middleware
-app.use(logRequest);
+function logRequest2(req, res, next) {
+	console.log("#2", req.method, req.url);
+	next();
+}
+
+/* LES MIDDLEWARES SONT UTLISE DANS L'ORDRE OU J'APPELLE APP.USE */
+
+// utilisation des middlewares
+app.use(logRequest1); // je passe la fonction directement (comme un callbakc)
+app.use(logRequest2);
+app.use(logRequest2);
+app.use(logRequest1); 
 
 // définition de la route
 app.get('/', (req, res) => {
 	res.send('Hello world');
+})
+
+app.get('/hey', (req, res) => {
+	res.send('HO');
 })
 
 app.listen(3000);
