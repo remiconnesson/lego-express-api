ed23c63a23d5ba4a93388c1b352c5c58406b0a82
Rémi Connesson <remiconnesson@gmail.com>
2022-05-05 17:23:20 +0200

    Code suffisant pour gérer les erreurs avec Express
    On n'a pas besoin de faire aussi compliqué que montré lors du cours.
    Ce qui est ici présent suffit.

--- 09-ERROR-MIDDLEWARE/app.js
+++ 09-ERROR-MIDDLEWARE/app.js
@@ -7,21 +7,13 @@ function myErrorMiddleware(err, req, res, next) {
 
 
 app.get('/', (req, res, next) => {
-	try {
 		throw new Error('ceci est un bug dans la route /');
 		res.send('Je ne serai pas executé du à un bug');
-	} catch(err) {
-		next(err)
-	}
 })
 
 app.get('/route2', (req, res, next) => {
-	try {
 		throw new Error('ceci est un bug dans la route 2');
 		res.send('Je ne serai pas executé du à un bug');
-	} catch(err) {
-		next(err)
-	}
 })
 
 app.use(myErrorMiddleware);
