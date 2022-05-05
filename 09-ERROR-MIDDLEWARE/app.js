a49974f8d3a8b95a6ef1f3b38ebac79554b842eb
Rémi Connesson <remiconnesson@gmail.com>
2022-05-05 13:56:42 +0200

    OK: Utilisation de trycatch et de next

--- 09-ERROR-MIDDLEWARE/app.js
+++ 09-ERROR-MIDDLEWARE/app.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const app = express();
 
-function myErrorMiddleware(err, req, res, next){
-	res.send(500).json({erreur: err.message});
+function myErrorMiddleware(err, req, res, next) {
+	res.status(500).json({erreur: err.message});
 }
 
-app.get('/', (req, res) => {
-	throw new Error('ceci est un bug');
+
+app.get('/', (req, res, next) => {
+	try {
+		throw new Error('ceci est un bug dans la route /');
+		res.send('Je ne serai pas executé du à un bug');
+	} catch(err) {
+		next(err)
+	}
+})
+
+app.get('/route2', (req, res, next) => {
+	try {
+		throw new Error('ceci est un bug dans la route 2');
+		res.send('Je ne serai pas executé du à un bug');
+	} catch(err) {
+		next(err)
+	}
 })
 
 app.use(myErrorMiddleware);
-
 
 
 app.listen(3000);
