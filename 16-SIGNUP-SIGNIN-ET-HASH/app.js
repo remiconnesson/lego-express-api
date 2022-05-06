7f4f6b140eeaf80433c9b29d0c30b7a0085218dc
Rémi Connesson <remiconnesson@gmail.com>
2022-05-06 10:19:38 +0200

    Vérif. du hash du password et emmission du token

--- 16-SIGNUP-SIGNIN-ET-HASH/app.js
+++ 16-SIGNUP-SIGNIN-ET-HASH/app.js
@@ -31,23 +31,20 @@ app.post("/signup", async (req, res) => {
   });
 
   const { value: account, error } = schema.validate(payload);
+  if (error) return res.status(400).send({ erreur: error.details[0].message });
+  // AVANT D'INSCRIRE ON VERIFIE QUE LE COMPTE EST UNIQUE.
+  const { id, found } = Accounts.findByProperty("email", account.email);
+  if (found) return res.status(400).send("Please signin instead of signup");
+  // WE NEED TO HASH THE PASSWORDW
+  const salt = await bcrypt.genSalt(10);
+  const passwordHashed = await bcrypt.hash(account.password, salt);
+  account.password = passwordHashed;
 
-  if (error) res.status(400).send({ erreur: error.details[0].message });
-  else {
-    // AVANT D'INSCRIRE ON VERIFIE QUE LE COMPTE EST UNIQUE.
-    const { id, found } = Accounts.findByProperty("email", account.email);
-    if (found) res.status(400).send("Please signin instead of signup");
-    // WE NEED TO HASH THE PASSWORDW
-    const salt = await bcrypt.genSalt(10);
-    const passwordHashed = await bcrypt.hash(account.password, salt);
-    account.password = passwordHashed;
-
-    Accounts.insertOne(account);
-    res.status(201).json({
-      name: account.name,
-      email: account.email,
-    });
-  }
+  Accounts.insertOne(account);
+  res.status(201).json({
+    name: account.name,
+    email: account.email,
+  });
 });
 
 app.post("/signin", async (req, res, next) => {
@@ -58,34 +55,23 @@ app.post("/signin", async (req, res, next) => {
   });
 
   const { value: connexion, error } = schema.validate(payload);
-  console.log("1", connexion);
 
-  if (error) res.status(400).send({ erreur: error.details[0].message });
-  else {
-    // ON CHERCHE LE COMPTE DANS LA DB
-    const { id, found: account } = Accounts.findByProperty(
-      "email",
-      connexion.email
-    );
-    console.log(id, account);
-    if (!account) {
-      res.status(400).send({ erreur: "Email Invalide" });
-      next();
-    } else {
-      // ON DOIT COMPARER LES HASH
-      const passwordIsValid = bcrypt.compare(req.body.password, account.password);
-      if (passwordIsValid)
-        res.status(400).send({ erreur: "Mot de Passe Invalide" });
-      else {
-        //ON RETOURNE UN JWT
-        const token = jwt.sign({ id }, process.env.JWT_PRIVATE_KEY);
-        res
-          .header("x-auth-token", token)
-          .status(200)
-          .send({ name: account.name });
-      }
-    }
-  }
+  if (error) return res.status(400).send({ erreur: error.details[0].message });
+  // ON CHERCHE LE COMPTE DANS LA DB
+  const { id, found: account } = Accounts.findByProperty(
+    "email",
+    connexion.email
+  );
+  if (!account) return res.status(400).send({ erreur: "Email Invalide" });
+
+  // ON DOIT COMPARER LES HASH
+  const passwordIsValid = bcrypt.compare(req.body.password, account.password);
+  if (!passwordIsValid)
+    return res.status(400).send({ erreur: "Mot de Passe Invalide" });
+
+  //ON RETOURNE UN JWT
+  const token = jwt.sign({ id }, process.env.JWT_PRIVATE_KEY);
+  res.header("x-auth-token", token).status(200).send({ name: account.name });
 });
 
 if (process.env.NODE_ENV !== "test") {
